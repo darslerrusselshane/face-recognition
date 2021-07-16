79816ace08fb55f52176063e183e140826c4bda9
Darsler <darsler@pm.me>
2021-07-16 18:35:47 +0800

    Update face_rec.py

--- face_rec.py
+++ face_rec.py
@@ -15,11 +15,11 @@ def get_encoded_faces():
     encoded = {}
 
     for dirpath, dnames, fnames in os.walk("./faces"):
-        for f in fnames:
-            if f.endswith(".jpg") or f.endswith(".png"):
-                face = fr.load_image_file("faces/" + f)
+        for face in fnames:
+            if facex.endswith(".jpg") or facex.endswith(".png"):
+                facex = fr.load_image_file("faces/" + f)
                 encoding = fr.face_encodings(face)[0]
-                encoded[f.split(".")[0]] = encoding
+                encoded[facex.split(".")[0]] = encoding
 
     return encoded
 
